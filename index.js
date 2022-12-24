45f862069e1550cc0953693c6f00b8de7ff40aef
Thien DAO <thien1997nus@gmail.com>
2022-12-24 19:55:52 +0700

    change html template to hbs handlebar. in views dir

--- index.js
+++ index.js
@@ -3,10 +3,23 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const expressHandlebars = require('express-handlebars');
 //cau hinh public static folder
 app.use( express.static(__dirname+ '/public'));
 
+//cau hinh su dung express-handlebars
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir:__dirname + '/views/partials',
+    extname: 'hbs',
+    defaultLayout: 'layout'
+}));
+app.set('view engine', 'hbs');
+
+//routes
+app.get('/', (req, res) => {
+    res.render('index');
+})
 //khoi dong web server
 app.listen( port, () =>{
     console.log(`server is running on port ${port}`);
