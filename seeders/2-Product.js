6f4c26e8426209c7ff03f7303f3e1904e7d269a6
Thien DAO <thien1997nus@gmail.com>
2023-01-14 22:48:18 +0700

    seeder cho brand, product. parent before child table

---
+++ seeders/2-Product.js
@@ -0,0 +1,87 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   let data = [{
+    "name": "placerat ante nulla",
+    "imagePath": "/img/product-1.png",
+    "oldPrice": 37.98,
+    "price": 29.69,
+    "summary": "Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio.",
+    "description": "Duis bibendum. Morbi non quam nec dui luctus rutrum. Nulla tellus.\n\nIn sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.",
+    "specification": "Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+    "stars": 3,
+    "quantity": 80,
+    "brandId": 5
+  }, {
+    "name": "quisque ut",
+    "imagePath": "/img/product-4.png",
+    "oldPrice": 11.74,
+    "price": 92.08,
+    "summary": "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.",
+    "description": "Sed ante. Vivamus tortor. Duis mattis egestas metus.\n\nAenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.",
+    "specification": "Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+    "stars": 2,
+    "quantity": 97,
+    "brandId": 1
+  }, {
+    "name": "hac habitasse platea",
+    "imagePath": "/img/product-5.png",
+    "oldPrice": 79.67,
+    "price": 22.69,
+    "summary": "In sagittis dui vel nisl. Duis ac nibh.",
+    "description": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.",
+    "specification": "Phasellus in felis. Donec semper sapien a libero. Nam dui.\n\nProin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.",
+    "stars": 5,
+    "quantity": 91,
+    "brandId": 6
+  }, {
+    "name": "sem sed sagittis",
+    "imagePath": "/img/product-2.png",
+    "oldPrice": 83.06,
+    "price": 49.03,
+    "summary": "Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla.",
+    "description": "Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.",
+    "specification": "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.",
+    "stars": 1,
+    "quantity": 52,
+    "brandId": 6
+  }, {
+    "name": "blandit mi in",
+    "imagePath": "/img/product-3.png",
+    "oldPrice": 30.16,
+    "price": 98.87,
+    "summary": "Nulla mollis molestie lorem. Quisque ut erat.",
+    "description": "Vestibulum ac est lacinia nisi venenatis tristique. Fusce congue, diam id ornare imperdiet, sapien urna pretium nisl, ut volutpat sapien arcu sed augue. Aliquam erat volutpat.\n\nIn congue. Etiam justo. Etiam pretium iaculis justo.\n\nIn hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.",
+    "specification": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.\n\nIn hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.",
+    "stars": 4,
+    "quantity": 95,
+    "brandId": 6
+  }];
+  data.forEach(item => {
+    item.createdAt = Sequelize.literal('NOW()');
+    item.updatedAt = Sequelize.literal('NOW()');
+  })
+  await queryInterface.bulkInsert('Products', data, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('Products', null, {});
+  }
+};
