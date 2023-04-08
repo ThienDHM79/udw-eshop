fda11906b4b536e1ec2b589d8350f7ab459bfa68
Thien DAO <thien1997nus@gmail.com>
2023-04-08 09:46:39 +0700

    show detail shopping list

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,7 +4,9 @@ let express = require('express');
 let router = express.Router();
 let controller = require('../controllers/productsController');
 let cartController = require('../controllers/cartController');
-router.get('/', controller.getData, controller.show )
+router.get('/', controller.getData, controller.show );
+
+router.get('/cart', cartController.show);
 router.get('/:id', controller.getData,controller.showDetails);
 
 router.post('/cart',cartController.add);
