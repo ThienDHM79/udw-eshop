74cc0e6ddf2afb66676058fcc21666200f4503c3
Thien DAO <thien1997nus@gmail.com>
2023-04-29 13:38:44 +0700

    payment placeorder

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -6,4 +6,5 @@ const router = express.Router();
 const controller = require('../controllers/usersController');
 
 router.get('/checkout', controller.checkout);
+router.post('/placeorders', controller.placeorders);
 module.exports = router;
